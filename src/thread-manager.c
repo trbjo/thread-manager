30c8fe73ed6faa0bef041ddcbead4ce561e88358
Troels Bjørnskov <troels@bjoernskov.org>
2025-01-17 18:04:35 +0100

    128 bit optimizations

--- src/thread-manager.c
+++ src/thread-manager.c
@@ -1,73 +1,66 @@
 #include <stdlib.h>
-#include <string.h>
-
+#include <pthread.h>
 #include "thread-manager.h"
 
-static ThreadPool* g_pool;
+static pthread_t* threads;
+static atomic_ulong __attribute__((aligned(CACHE_LINE_SIZE))) assigned = 0;
+static atomic_ulong __attribute__((aligned(CACHE_LINE_SIZE))) scheduled = 0;
+static uint128_t_padded slots[MAX_SLOTS] __attribute__((aligned(CACHE_LINE_SIZE)));
 
 static void* worker_thread(void* arg) {
-    CacheAligned128_t* slots = g_pool->slots;
-    atomic_ulong* next = &g_pool->next;
-    uint16_t chunk = WRAP_SLOT(a_inc(next));
-
+    uint16_t chunk = WRAP_SLOT(a_inc(&assigned));
     while (1) {
-        uint128_t old_value = atomic128_load(&slots[chunk].value);
-        if (old_value == DONE) return NULL;
+        uint128_t task = atomic128_load(&slots[chunk]);
+        if (task == DONE) return NULL;
 
-        if (old_value != EMPTY && atomic128_compare_exchange(&slots[chunk].value, &old_value, EMPTY)) {
-            TaskFunc func = (TaskFunc)((uint64_t)old_value & VIRTUAL_ADDRESS_MASK);
-            TaskDestroy destroy = (TaskDestroy)(func + (int16_t)((uint64_t)old_value >> 48));
-            void* data = (void*)(uintptr_t)(old_value >> 64);
+        if (task != EMPTY && a128_cmp_exchange(&slots[chunk], &task, EMPTY)) {
+            TaskFunc func = (TaskFunc)((uintptr_t)task & VIRTUAL_ADDRESS_MASK);
+            TaskDestroy destroy = (TaskDestroy)(func + (int16_t)((uintptr_t)task >> 48));
+            void* data = (void*)(uintptr_t)(task >> 64);
 
             func(data);
             if (destroy) destroy(data);
-            chunk = WRAP_SLOT(a_inc(next));
-        } else if (atomic128_compare_exchange(&slots[chunk].value, &old_value, DONE)) {
-            syscall(SYS_futex, &slots[chunk].value, FUTEX_WAIT_PRIVATE, DONE, NULL, NULL, 0);
+            chunk = WRAP_SLOT(a_inc(&assigned));
+        } else if (task == EMPTY && a128_cmp_exchange(&slots[chunk], &task, DONE)) {
+            syscall(SYS_futex, &slots[chunk], FUTEX_WAIT_PRIVATE, DONE, NULL, NULL, 0);
         }
     }
 }
 
-void thread_pool_run(TaskFunc task, void* task_user_data, TaskDestroy task_destroy) {
-    uint64_t low = (uint64_t)task | ((uint64_t)(task_destroy - task) << 48);
-    uint128_t new_value = (((uint128_t)(uint64_t)task_user_data) << 64) | low;
+void thread_pool_run(TaskFunc func, void* data, TaskDestroy destroy) {
+    uint128_t new_value = ((uint128_t)(uintptr_t)data << 64) | ((uintptr_t)(destroy - func) << 48) | ((uintptr_t)func);
 
-    const uint16_t chunk = WRAP_SLOT(a_inc(&g_pool->scheduled));
+    const uint16_t chunk = WRAP_SLOT(a_inc(&scheduled));
 
-    uint128_t expected = atomic128_load(&g_pool->slots[chunk].value);
-    do {
+    uint128_t expected = load_lo(&slots[chunk]) == DONE ? DONE : EMPTY;
+    while (!a128_cmp_exchange(&slots[chunk], &expected, new_value)) {
         expected = expected == DONE ? DONE : EMPTY;
-    } while (!atomic128_compare_exchange(&g_pool->slots[chunk].value, &expected, new_value));
+    }
 
     if (expected == DONE) {
-        syscall(SYS_futex, &g_pool->slots[chunk].value, FUTEX_WAKE_PRIVATE, 1, NULL, NULL, 0);
+        syscall(SYS_futex, &slots[chunk], FUTEX_WAKE_PRIVATE, 1, NULL, NULL, 0);
     }
 }
 
-void thread_pool_initialize(void) {
-    g_pool = aligned_alloc(CACHE_LINE_SIZE, sizeof(ThreadPool));
-    if (!g_pool) abort();
-    memset(g_pool, 0, sizeof(ThreadPool));
+void thread_pool_initialize() {
+    const int num_threads = sysconf(_SC_NPROCESSORS_CONF);
+    threads = malloc(num_threads * sizeof(pthread_t));
 
-    g_pool->max_threads = sysconf(_SC_NPROCESSORS_CONF);
-    g_pool->threads = malloc(sizeof(pthread_t) * g_pool->max_threads);
-
-    for (int i = 0; i < g_pool->max_threads; i++) {
-        pthread_create(&g_pool->threads[i], NULL, worker_thread, NULL);
+    for (int i = 0; i < num_threads; i++) {
+        pthread_create(&threads[i], NULL, worker_thread, NULL);
     }
 }
 
-void thread_pool_destroy(void) {
-    uint64_t lowest = a_load(&g_pool->scheduled);
-    while (lowest <= a_load(&g_pool->next)) {
+void thread_pool_destroy() {
+    uint64_t lowest = a_load(&scheduled);
+    while (lowest <= a_load(&assigned)) {
         uint16_t chunk = WRAP_SLOT(lowest++);
-        syscall(SYS_futex, &g_pool->slots[chunk].value, FUTEX_WAKE_PRIVATE, 1, NULL, NULL, 0);
+        syscall(SYS_futex, &slots[chunk], FUTEX_WAKE_PRIVATE, 1, NULL, NULL, 0);
     }
 
-    for (int i = 0; i < g_pool->max_threads; i++) {
-        pthread_join(g_pool->threads[i], NULL);
+    for (int i = 0; i < sysconf(_SC_NPROCESSORS_CONF); i++) {
+        pthread_join(threads[i], NULL);
     }
 
-    free(g_pool->threads);
-    free(g_pool);
+    free(threads);
 }
--- src/thread-manager.h
+++ src/thread-manager.h
@@ -2,81 +2,84 @@
 #define THREAD_MANAGER_H
 
 #include <stdatomic.h>
-#include <pthread.h>
 #include <unistd.h>
 #include <stdint.h>
 #include <sys/syscall.h>
 #include <linux/futex.h>
-#include <immintrin.h>
 
+#define uint128_t __uint128_t
 #define MAX_SLOTS 512
 #define CACHE_LINE_SIZE 64
 #define WRAP_SLOT(x) ((x) & (MAX_SLOTS - 1))
 #define EMPTY 0
 #define DONE 1
+
 #define VIRTUAL_ADDRESS_MASK 0xFFFFFFFFFFFFULL
 
 #define a_load(ptr)         atomic_load_explicit((ptr), memory_order_relaxed)
 #define a_inc(ptr)          atomic_fetch_add_explicit((ptr), 1ULL, memory_order_relaxed)
-#define a_cmp_exchange(ptr, val, des) atomic_compare_exchange_weak_explicit((ptr), (val), (des), memory_order_relaxed, memory_order_relaxed)
-#define ALWAYS_INLINE __attribute__((__always_inline__))
-#define uint128_t __uint128_t
-
-typedef void (*TaskFunc)(void* user_data);
-typedef void (*TaskDestroy)(void* data);
 
 typedef struct {
-    _Alignas(16) uint128_t value;
+    uint128_t value;
     char padding[CACHE_LINE_SIZE - sizeof(uint128_t)];
-} CacheAligned128_t;
+} __attribute__((aligned(CACHE_LINE_SIZE))) uint128_t_padded;
 
-struct ThreadPool {
-    _Alignas(CACHE_LINE_SIZE) atomic_ulong scheduled;
-    char pad1[CACHE_LINE_SIZE - sizeof(atomic_ulong)];
-
-    _Alignas(CACHE_LINE_SIZE) atomic_ulong next;
-    char pad2[CACHE_LINE_SIZE - sizeof(atomic_ulong)];
-
-    _Alignas(CACHE_LINE_SIZE) CacheAligned128_t slots[MAX_SLOTS];
-
-    pthread_t* threads;
-    int max_threads;
-} __attribute__((aligned(CACHE_LINE_SIZE)));
-
-typedef struct ThreadPool ThreadPool;
-
-void thread_pool_initialize(void);
-void thread_pool_destroy(void);
-void thread_pool_run(TaskFunc task, void* task_user_data, TaskDestroy task_destroy);
-
-ALWAYS_INLINE static inline int atomic128_compare_exchange(
-    uint128_t *ptr,
+static inline int __a128_cmp_exchange(
+    uint128_t *address,
     uint128_t *expected,
     uint128_t desired
 ) {
     int success;
+    uint64_t *exp_lo = (uint64_t *)expected;
+    uint64_t *exp_hi = exp_lo + 1;
     __asm__ __volatile__ (
         "lock; cmpxchg16b %0"
-        : "+m" (*ptr),
-          "+d" (*(uint64_t*)((char*)expected + 8)),
-          "+a" (*(uint64_t*)expected),
+        : "+m" (*address),
+          "+d" (*exp_hi),
+          "+a" (*exp_lo),
           "=@ccz" (success)
         : "c" ((uint64_t)(desired >> 64)),
           "b" ((uint64_t)desired)
-        : "cc"
+        : "cc", "memory"
     );
     return success;
 }
 
-ALWAYS_INLINE static inline uint128_t atomic128_load(uint128_t const *ptr) {
-    uint128_t result;
+#define a128_cmp_exchange(ptr, expected, desired) \
+    __a128_cmp_exchange(((uint128_t*)ptr), expected, desired)
+
+static inline __uint128_t __atomic128_load(__uint128_t const *address) {
+    __uint128_t result;
     __asm__ __volatile__ (
         "movdqa %1, %0"
         : "=x" (result)
+        : "m" (*address)
+        : "memory"
+    );
+    return result;
+}
+
+#define atomic128_load(ptr) \
+    __atomic128_load(((uint128_t*)ptr))
+
+static inline uint64_t __load_lo(uint128_t* ptr) {
+    uint64_t result;
+    __asm__ __volatile__(
+        "movq %1, %0"
+        : "=r" (result)
         : "m" (*ptr)
         : "memory"
     );
     return result;
 }
+
+#define load_lo(ptr) __load_lo(((uint128_t*)ptr))
+
+typedef void (*TaskFunc)(void* user_data);
+typedef void (*TaskDestroy)(void* data);
+
+void thread_pool_initialize();
+void thread_pool_destroy();
+void thread_pool_run(TaskFunc task, void* task_user_data, TaskDestroy task_destroy);
 
 #endif
